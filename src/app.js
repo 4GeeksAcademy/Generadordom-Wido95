9e571ea49c8cc896f93bb9d6db85fcc50f6eb629
Guido Fernan Alonso <141326682+Wido95@users.noreply.github.com>
2023-10-16 14:59:51 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let exten = [".es", ".com"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let h = 0; h < noun.length; h++) {
+      for (let k = 0; k < exten.length; k++) {
+        let div = document.createElement("div");
+        div.innerText = `
+      ${pronoun[i]}${adj[j]}${noun[h]}${exten[k]}
+      `;
+        document.body.appendChild(div);
+      }
+    }
+  }
+}
